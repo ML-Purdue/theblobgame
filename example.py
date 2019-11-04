8ed4d14e5786dad8c7809b82f8df7b014f5b6447
Krithik Rao <krdevmail@gmail.com>
2019-11-04 17:15:04 -0500

    Closest Food Finding Example

--- example.py
+++ example.py
@@ -1,14 +1,24 @@
 import socketio
 import asyncio
 import random
+import math
 
 sio = socketio.AsyncClient()
 
+def dist_between_blobs(a,b):
+    x1, y1 = a["coords"]
+    x2, y2 = b["coords"]
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) **2)
 
 async def action_request(data):
     state = data["state"]
     my_blobs = data["my_blobs"]
     state_id = data["state_id"]
+    food = [o for o in state if o["is_food"]]
+    closest_food = min(food, key=lambda x: dist_between_blobs(x, my_blobs[0]))
+
+    food_x = closest_food["coords"][0]-my_blobs[0]["coords"][0]
+    food_y = closest_food["coords"][1]-my_blobs[0]["coords"][1]
 
     await sio.emit(
         "action",
@@ -17,8 +27,8 @@ async def action_request(data):
             "actions": [
                 {
                     "blob_id": my_blobs[0]["blob_id"],
-                    "vector": [random.uniform(-1, 1), random.uniform(-1, 1)],
-                    "speed": 50,
+                    "vector": [food_x, food_y],
+                    "speed": 30,
                     "type": "move",
                 }
             ],
@@ -31,7 +41,7 @@ async def error_handler(data):
 
 
 async def start(sio):
-    await sio.connect("http://142.93.112.226:4001")
+    await sio.connect("http://142.93.112.226:4000")
     await sio.emit("register", {"name": "elnardu" + str(random.randint(1, 100))})
     await sio.wait()
 
