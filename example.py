83906532503027db142fe28788a2ac0105b71bee
Krithik Rao <krdevmail@gmail.com>
2019-11-18 17:35:58 -0500

    Committed Some Version of Pathfinding to Food

--- example.py
+++ example.py
@@ -7,22 +7,38 @@ import numpy as np
 
 sio = socketio.AsyncClient()
 
-DIRECTIONS = [(-1, 0), (-1, -1), (0, 1), (1, 1), (1,  0), (1, -1), (0, -1), (-1, -1)]
+DIRECTIONS = [(-1, 0), (-1, -1), (0, 1), (1, 1), (1,  0), (1, -1), (0, -1), (-1, 1)]
 
-def breadth_first(graph, start, end):
+def euc_dist(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
+def a_star(graph, start, end):
+    #print(start)
+    #print(end)
+    #print("Begin")
     graph = np.copy(graph)
-    q = queue.Queue()
-    q.put(start)
+    cost_so_far = np.zeros(graph.shape)
+    q = queue.PriorityQueue()
+    graph[start[0], start[1]] = -1
+    q.put((0,start))
     while not q.empty():
-        curr = q.get()
+        #print(q.queue)
+        curr = q.get()[1]
+        #print("curr", curr)
+        #input()
+        curr_cost = cost_so_far[curr[0], curr[1]] 
         for i, direction in enumerate(DIRECTIONS):
             nextLoc = (curr[0] + direction[0], curr[1] + direction[1])
-            if isValidPoint(graph, nextLoc) and graph[nextLoc[0], nextLoc[1]] == 0:
-                q.put(nextLoc)
+            cost = math.sqrt((direction[0] ** 2) + (direction[1] ** 2)) + curr_cost
+            if isValidPoint(graph, nextLoc) and ((graph[nextLoc[0], nextLoc[1]] == 0) or (cost < cost_so_far[nextLoc[0], nextLoc[1]])):
+                #print("Updated")
+                priority = cost + curr_cost + euc_dist(nextLoc, end)
+                #print(direction, euc_dist(nextLoc, end), priority)
+                q.put((priority, nextLoc))
+                cost_so_far[nextLoc[0], nextLoc[1]] = cost + curr_cost
                 graph[nextLoc[0], nextLoc[1]] = i + 1
                 if nextLoc == end:
                     return graph
-    print(graph)
     return graph
 
 def isValidPoint(graph, p):
@@ -58,11 +74,10 @@ def fill_grid(grid, enemies):
         right = math.min(grid.shape[0] - 1, x + radius)
         bottom = math.min(grid.shape[1] - 1, y + radius)
         grid[top:bottom, left:right] = -1
-    print(grid)
 
 async def action_request(data):
     # define grid
-    grid = np.zeros((1000,1000))
+    grid = np.zeros((1000,1000), dtype=int)
 
     state = data["state"]
     my_blobs = data["my_blobs"]
@@ -80,10 +95,17 @@ async def action_request(data):
 
     blob_coords = [int(x) for x in my_blobs[0]["coords"]]
     target_coords = [int(x) for x in target["coords"]]
+    
 
-    traced_graph = breadth_first(grid, blob_coords, target_coords)
+    traced_graph = a_star(grid, (blob_coords[1], blob_coords[0]), (target_coords[1], target_coords[0]))
+    
+    print("Breadth first complete")
      
     path = backtrace(traced_graph, target_coords)
+
+    print("Backtrace Complete")
+    print(path)
+    
 
     if len(path) > 1:
         f_x, f_y = path[-1]
@@ -114,7 +136,7 @@ async def error_handler(data):
 
 
 async def start(sio):
-    await sio.connect("http://142.93.112.226:4000")
+    await sio.connect("http://142.93.112.226:4004")
     await sio.emit("register", {"name": "elnardu" + str(random.randint(1, 100))})
     await sio.wait()
 
